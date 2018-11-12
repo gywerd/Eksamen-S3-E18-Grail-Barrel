e358c5ca467aa4db205212809bc8cd8b45da24ac
gywerd <gywerd@gmail.com>
2018-11-12 12:12:45 +0100

    OpenExchangeWebservice - Code Added

---
+++ EksamenS3_AspIT_2018_Daniel_Giversen/PurchaseIO/OpenExchangeWebService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Repository;
+
+namespace PurchaseIO
+{
+    public class OpenExchangeWebService
+    {
+        string url = "https://openexchangerates.org/api/";
+        private const string APP_ID = "5cf47e89535d41d9a7eb81d950673c02"; //Daniel Api
+        string responseStream = "";
+        //private HttpClient client;
+
+        public OpenExchangeWebService()
+        {
+            //client = new HttpClient();
+            //client.BaseAddress = new Uri(url);
+            //GetDataFromApiAsync();
+            GetDataFromApi();
+        }
+
+        public DollarRates GetCurrencyFromOpenExchangeRate()
+        {
+            //while (responseStream == "")
+            //{
+            //    Thread.Sleep(50);
+            //}
+
+            return JsonConvert.DeserializeObject<DollarRates>(responseStream);
+        }
+
+        //public async void GetDataFromApiAsync()
+        //{
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            string query = $"latest.json?app_id=" + APP_ID;
+        //            HttpResponseMessage responseMessageExchange = await client.GetAsync(query);
+
+        //            responseStream = await responseMessageExchange.Content.ReadAsStringAsync();
+
+        //            await Task.Delay(11000);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.Message);
+        //        }
+        //    }
+        //}
+
+        private void GetDataFromApi()
+        {
+            Uri uri = new Uri(url + $"latest.json?app_id=" + APP_ID);
+            using (WebClient w = new WebClient())
+            {
+                w.Headers["User-Agent"] = "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+
+                // attempt to download JSON data as a string
+                try
+                {
+                    this.responseStream = w.DownloadString(uri);
+                }
+                catch (Exception) { }
+            }
+        }
+        public string ResponseStream { get => this.responseStream; set => this.responseStream = value; }
+
+    }
+}
---
+++ EksamenS3_AspIT_2018_Daniel_Giversen/Repository/DollarRates.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DollarRates
+    {
+        #region Fields
+        private List<Rate> rateList;
+        private string _disclaimer;
+        private string _licence;
+        private string _timestamp;
+        private string _base;
+        private Dictionary<string, decimal> _rates;
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Empty Constructor
+        /// </summary>
+        public DollarRates()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Method, that fills Dollar Rate List with data
+        /// </summary>
+        public void FillListWithDollarRates()
+        {
+            List<Rate> ld = new List<Rate>();
+            foreach (var item in _rates)
+            {
+                Rate cd = new Rate();
+                cd.CurrencyCode = item.Key;
+                cd.Value = Convert.ToDouble(item.Value);
+                ld.Add(cd);
+            }
+
+            rateList = ld;
+        }
+
+        /// <summary>
+        /// Method, that browses the Rate List to find a specific value
+        /// </summary>
+        /// <param name="currencyCode">string</param>
+        /// <returns>string</returns>
+        public string GetRateFromCountryCode(string currencyCode)
+        {
+            foreach (Rate rate in rateList)
+            {
+                if (rate.CurrencyCode == currencyCode)
+                {
+                    return rate.ToString();
+                }
+            }
+            return "The requested currency, " + currencyCode + " is unknown.";
+        }
+
+        #endregion
+
+        #region Properties
+        public List<Rate> RateList
+        {
+            get { return this.rateList; }
+            set { this.rateList = value; }
+        }
+
+        public string disclaimer
+        {
+            get { return this._disclaimer; }
+            set { this._disclaimer = value; }
+        }
+
+        public string licence
+        {
+            get { return this._licence; }
+            set { this._licence = value; }
+        }
+
+
+        public string timestamp
+        {
+            get { return this._timestamp; }
+            set { this._timestamp = value; }
+        }
+
+        public string @base
+        {
+            get { return this._base; }
+            set { this._base = value; }
+        }
+
+        public Dictionary<string, decimal> rates
+        {
+            get { return this._rates; }
+            set
+            {
+                if (value != _rates)
+                {
+                    this._rates = value;
+                    FillListWithDollarRates();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
